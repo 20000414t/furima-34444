be8719cbdb55c5e11fe3e2a188de1f24eecd21cc
20000414t <78241361+20000414t@users.noreply.github.com>
2021-03-01 14:19:02 +0900

    修正
    指摘箇所の修正
    
    名前の表示
    with_optionsの使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
-         validates :password, presence: true,
+         with_options presence: true do
+         validates :password, 
             format: { with: /\A[a-z\d]{6,100}+\z/i}
-         validates :name_family, presence: true,
+         validates :name_family,
          format: {
            with: /\A[一-龥ぁ-ん]/}
-           validates :name_first, presence: true,
+           validates :name_first,
          format: {
            with: /\A[一-龥ぁ-ん]/}
-           validates :name_family_kana, presence: true,
+           validates :name_family_kana,
          format: {
            with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/}
-           validates :name_first_kana, presence: true,
+           validates :name_first_kana,
          format: {
            with:/[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/}
-           
+         end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
     name              {"tarou"}
-    email                 {"sample@gmail.com"}
+    email                 {"sample1@gmail.com"}
     password              {"1234567"}
     encrypted_password    {"1234567"}
     name_family           {"山田"}
     name_family_kana      {"ヤマダ"}
-    name_first            {"太郎"}
-    name_first_kana       {"タロウ"}
+    name_first            {"二郎"}
+    name_first_kana       {"ジロウ"}
     birthday             {"2000-01-01"}
   end
 end
