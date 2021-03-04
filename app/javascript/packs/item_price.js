4acce0ade0efbfd2a295c98cada7f31e96e15d2f
20000414t <78241361+20000414t@users.noreply.github.com>
2021-03-04 19:06:19 +0900

    JSの計算完了

--- app/javascript/packs/item_price.js
+++ app/javascript/packs/item_price.js
@@ -1,5 +1,13 @@
-const priceInput = document.getElementById(price_id);
-priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.Value;
-  console.log(inputValue);
-})
+window.addEventListener('load', () => {
+  console.log("OK");
+  const priceInput = document.getElementById("item-price")
+  console.log(priceInput);
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = inputValue * 0.1
+    const profit = document.getElementById("profit");
+    profit.innerHTML =  priceInput.value - inputValue * 0.1
+  })
+});
