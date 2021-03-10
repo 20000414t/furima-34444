76c9755d18985c27c5d37f9e7bbcbdd15a3e7d76
20000414t <78241361+20000414t@users.noreply.github.com>
2021-03-10 15:50:06 +0900

    完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, expect: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update]
   
   def index
@@ -32,6 +32,13 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    @items.destroy
+    if@items.destroy
+      redirect_to root_path
     end
   end
 
